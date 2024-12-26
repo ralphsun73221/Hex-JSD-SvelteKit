2c3f37d64a0f7af501ce1cfb0526d7be5f220a01
Ralph <my@ralphsun.me>
2024-12-26 14:44:20 +0800

    Changed: Clock.svelte favicon fix

--- src/lib/Clock.svelte
+++ src/lib/Clock.svelte
@@ -48,7 +48,7 @@
 <svelte:head>
 	<link
 		rel="icon"
-		href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>s🕒</text></svg>"
+		href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🕒</text></svg>"
 	/>
 	<title>時鐘</title>
 </svelte:head>
