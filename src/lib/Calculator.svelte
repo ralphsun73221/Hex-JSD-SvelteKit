0980c44afb392d2c37993d23c1c10dc6116ec74c
孫證傑 <sunzhengjie@RALPH-M2-Max.local>
2025-01-07 16:13:03 +0800

    Added: css html

--- src/lib/Calculator.svelte
+++ src/lib/Calculator.svelte
@@ -2,7 +2,28 @@
 	let number = $state(0);
 </script>
 
-<input type="text" bind:value={number} />
+<main>
+	<div class="bg">
+		<div class="results"></div>
+		<div class="button"></div>
+	</div>
+</main>
 
 <style lang="scss">
+	main {
+		margin: 0 auto;
+		align-content: center;
+		width: 1280px;
+		height: 800px;
+		background-color: #e8e8e8;
+
+		.bg {
+			margin: 0 auto;
+			width: 350px;
+			height: 525px;
+			background-color: #062145;
+			box-shadow: 0px 20px 40px #00000066;
+			border-radius: 20px;
+		}
+	}
 </style>
